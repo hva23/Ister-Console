a7bbf4f48440a100fa1b84d140f208744152665a
hva23 <haskihossein@gmail.com>
2023-09-09 14:46:28 +0330

    Update UserService.java

--- src/main/java/com/ister/service/UserService.java
+++ src/main/java/com/ister/service/UserService.java
@@ -1,22 +1,73 @@
 package com.ister.service;
 
+import com.ister.common.RequestStatus;
 import com.ister.domain.User;
-import com.ister.repository.UserJdbcRepositoryImpl;
-import com.ister.repository.UserRepository;
+import com.ister.repository.*;
 
 public class UserService {
 
     private UserRepository userRepository;
+    private ThingsRepository thingsRepository;
 
     public UserService() {
-        this.userRepository = new UserJdbcRepositoryImpl();
+        this.userRepository = new UserInMemoryRepositoryImpl();
+        this.thingsRepository = new ThingsInMemoryRepositoryImpl();
     }
 
-    public void signUp(User user) {
+    public RequestStatus signUp(User user) {
+        try{
+            userRepository.findById(user.getId());
+            System.out.printf("A user exists with this ID : %d", user.getId());
+            return RequestStatus.Failed;
+        }catch (Exception ex){
+            userRepository.create(user);
+        }
+        return RequestStatus.Successful;
+    }
+
+    public RequestStatus login(User user){
+        try{
+            userRepository.findById(user.getId());
+            System.out.printf("""
+                Welcome %s
+                you logged in with this email : %s
+                """, user.getUsername(), user.getEmail());
+            return RequestStatus.Successful;
+        }catch (Exception ex){
+            System.out.printf("A user exists with this ID : %d", user.getId());
+            return RequestStatus.Failed;
+        }
 
     }
 
-    public void delete(User user) {
+    public RequestStatus forgotPassword(User user){
+        System.out.printf("""
+                Your password : %s
+                """, user.getPassword());
+        return RequestStatus.Successful;
+    }
+
+    public RequestStatus editProfile(User user){
+        try{
+            userRepository.findById(user.getId());
+            userRepository.delete(user);
+            userRepository.create(user);
+        }catch (Exception ex){
+            System.out.println("This user doesn't exists!");
+        }
+        return RequestStatus.Successful;
+    }
+
+    public RequestStatus delete(User user) {
+        try{
+            userRepository.findById(user.getId());
+            userRepository.delete(user);
+            thingsRepository.delete(thingsRepository.findByUser(user));
+            return RequestStatus.Successful;
+        }catch (Exception ex){
+            System.out.println("This user doesn't exists or something went wrong!");
+            return RequestStatus.Failed;
+        }
 
     }
 }
