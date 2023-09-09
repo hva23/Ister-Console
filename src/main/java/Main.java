de74965980e2f2d323c0c913593f4ae43f8662c4
hva23 <haskihossein@gmail.com>
2023-09-09 14:44:42 +0330

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import com.ister.domain.User;
+import com.ister.repository.UserInMemoryRepositoryImpl;
+import com.ister.repository.UserRepository;
+
+public class Main {
+    public static void main(String args[]){
+    }
+}
