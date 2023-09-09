7efd202d13a6ad7c1d30313c87afb280cd6bb51b
hva23 <haskihossein@gmail.com>
2023-09-09 14:46:04 +0330

    Update ThingsRepository.java

--- src/main/java/com/ister/repository/ThingsRepository.java
+++ src/main/java/com/ister/repository/ThingsRepository.java
@@ -1,2 +1,28 @@
-package com.ister.repository;public interface ThingsRepository {
+package com.ister.repository;
+
+import com.ister.domain.TelemetryData;
+import com.ister.domain.Things;
+import com.ister.domain.User;
+
+import java.util.List;
+
+public interface ThingsRepository {
+    void create(Things thing);
+
+    void delete(Things thing);
+    void delete(List<Things> things);
+
+    List<Things> getAll();
+
+    List<TelemetryData> getAllRecords();
+
+    TelemetryData getThingRecord(Things thing);
+
+    Things findById(Long id);
+
+    Things findBySerialNumber(String serialNumber);
+
+    List<Things> findByUser(User user);
+
+
 }
