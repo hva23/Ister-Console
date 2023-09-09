0b05f5e880aadbc80332bb63c0c5b15b65c34327
hva23 <haskihossein@gmail.com>
2023-09-09 14:44:59 +0330

    Update ThingsInMemoryRepositoryImpl.java

--- src/main/java/com/ister/repository/ThingsInMemoryRepositoryImpl.java
+++ src/main/java/com/ister/repository/ThingsInMemoryRepositoryImpl.java
@@ -1,2 +1,57 @@
-package com.ister.repository;public class ThingsInMemoryRepository {
+package com.ister.repository;
+
+import com.ister.domain.TelemetryData;
+import com.ister.domain.Things;
+import com.ister.domain.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThingsInMemoryRepositoryImpl implements ThingsRepository{
+
+    List<Things> thingsInMemory = new ArrayList<>();
+    @Override
+    public void create(Things thing) {
+        thingsInMemory.add(thing);
+    }
+
+    @Override
+    public void delete(Things thing) {
+        thingsInMemory.remove(thing);
+    }
+
+    @Override
+    public void delete(List<Things> things){
+
+    }
+
+    @Override
+    public List<Things> getAll() {
+        return thingsInMemory;
+    }
+
+    @Override
+    public List<TelemetryData> getAllRecords() {
+        return null;//thingsInMemory.stream().filter(t -> t.getAttributes())
+    }
+
+    @Override
+    public TelemetryData getThingRecord(Things thing) {
+        return null;
+    }
+
+    @Override
+    public Things findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Things findBySerialNumber(String serialNumber) {
+        return null;
+    }
+
+    @Override
+    public List<Things> findByUser(User user){
+        return null;
+    }
 }
