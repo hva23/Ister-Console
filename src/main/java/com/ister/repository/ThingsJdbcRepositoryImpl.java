b48e3677bb0c912a89c67650695a4ebecf5d3257
hva23 <haskihossein@gmail.com>
2023-09-09 14:45:01 +0330

    Update ThingsJdbcRepositoryImpl.java

--- src/main/java/com/ister/repository/ThingsJdbcRepositoryImpl.java
+++ src/main/java/com/ister/repository/ThingsJdbcRepositoryImpl.java
@@ -1,2 +1,55 @@
-package com.ister.repository;public class ThingsJdbcRepositoryImpl {
+package com.ister.repository;
+
+import com.ister.domain.TelemetryData;
+import com.ister.domain.Things;
+import com.ister.domain.User;
+
+import java.util.List;
+
+public class ThingsJdbcRepositoryImpl implements ThingsRepository{
+    @Override
+    public void create(Things thing) {
+
+    }
+
+    @Override
+    public void delete(Things thing) {
+
+    }
+
+    @Override
+    public void delete(List<Things> things){
+
+    }
+
+    @Override
+    public List<Things> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<TelemetryData> getAllRecords() {
+        return null;
+    }
+
+    @Override
+    public TelemetryData getThingRecord(Things thing) {
+        return null;
+    }
+
+    @Override
+    public Things findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Things findBySerialNumber(String serialNumber) {
+        return null;
+    }
+
+
+    @Override
+    public List<Things> findByUser(User user){
+        return null;
+    }
 }
