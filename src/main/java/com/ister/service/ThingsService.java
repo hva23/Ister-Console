1ea6b4eca86d01cbc2480c99cdf29fbf28c8acb3
hva23 <haskihossein@gmail.com>
2023-09-11 10:49:45 +0330

    Update ThingsService.java

--- src/main/java/com/ister/service/ThingsService.java
+++ src/main/java/com/ister/service/ThingsService.java
@@ -7,46 +7,48 @@ import com.ister.domain.User;
 import com.ister.repository.ThingsInMemoryRepositoryImpl;
 import com.ister.repository.ThingsRepository;
 
+import java.util.Optional;
+
 public class ThingsService {
     ThingsRepository thingsRepository = new ThingsInMemoryRepositoryImpl();
-    public RequestStatus addThing(Things thing){
-        try{
-            thingsRepository.findById(thing.getId());
+
+    public RequestStatus addThing(Things thing) {
+        Optional<Things> dbThingOptional = thingsRepository.findById(thing.getId());
+        if (dbThingOptional.isPresent()) {
             System.out.printf("A thing exists with this ID : %d", thing.getId());
             return RequestStatus.Failed;
-        }catch (Exception ex){
-            thingsRepository.create(thing);
         }
+        thingsRepository.create(thing);
         return RequestStatus.Successful;
     }
 
-    public RequestStatus editThing(Things thing){
-        try{
+    public RequestStatus editThing(Things thing) {
+        try {
             thingsRepository.findById(thing.getId());
             thingsRepository.delete(thing);
             thingsRepository.create(thing);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println("This user doesn't exists!");
         }
         return RequestStatus.Successful;
     }
 
-    public RequestStatus delete(Things thing){
-        try{
+    public RequestStatus delete(Things thing) {
+        try {
             thingsRepository.findById(thing.getId());
             thingsRepository.delete(thing);
             return RequestStatus.Successful;
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.out.println("This thing doesn't exists or something went wrong!");
             return RequestStatus.Failed;
         }
     }
 
-    public TelemetryData getData(Things thing){
+    public TelemetryData getData(Things thing) {
         return null;
     }
 
-    public RequestStatus sendData(Things thing){
+    public RequestStatus sendData(Things thing) {
         return RequestStatus.Successful;
     }
 }
