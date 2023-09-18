86f058e735ea3c0b18299e13cea84b41a380e271
hva23 <haskihossein@gmail.com>
2023-09-18 14:07:18 +0330

    Update TelemetryData.java

--- src/main/java/com/ister/domain/TelemetryData.java
+++ src/main/java/com/ister/domain/TelemetryData.java
@@ -2,7 +2,7 @@ package com.ister.domain;
 
 import java.util.Map;
 
-public class TelemetryData extends BaseEntity {
+public class TelemetryData extends BaseEntity<Long> {
     private Map<String, Object> data;
     private Things thing;
 
