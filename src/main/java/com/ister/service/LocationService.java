f4ce28e06e0e4f685b55f4def9e55c9ed120bea9
hva23 <haskihossein@gmail.com>
2023-09-19 17:54:24 +0330

    Update LocationService.java

--- src/main/java/com/ister/service/LocationService.java
+++ src/main/java/com/ister/service/LocationService.java
@@ -2,28 +2,103 @@ package com.ister.service;
 
 import com.ister.common.RequestStatus;
 import com.ister.domain.Location;
-import com.ister.repository.LocationJdbcRepository;
-import com.ister.repository.ThingsJdbcRepositoryImpl;
+import com.ister.repository.LocationJdbcRepositoryImpl;
+
+import java.util.*;
 
 public class LocationService {
-    LocationJdbcRepository locationJdbcRepository;
+    LocationJdbcRepositoryImpl locationJdbcRepositoryImpl;
+
     public LocationService() {
-        this.locationJdbcRepository = new LocationJdbcRepository("jdbc:mysql://localhost:8080/Ister", "root", "v@h@bI2442");
+        this.locationJdbcRepositoryImpl = new LocationJdbcRepositoryImpl("jdbc:mysql://localhost:8080/Ister", "root", "v@h@bI2442");
     }
+
     public RequestStatus addLocation(Location location) {
-        return locationJdbcRepository.create(location) ? RequestStatus.Successful : RequestStatus.Failed;
+        if (locationJdbcRepositoryImpl.findById(location.getId()).isPresent()) {
+            System.out.println("The location already exists\nOperation failed");
+            return RequestStatus.Failed;
+        } else {
+            if (locationJdbcRepositoryImpl.create(location)) {
+                System.out.println("Location added successfully");
+                return RequestStatus.Successful;
+            } else {
+                System.out.println("Something went wrong!");
+                return RequestStatus.Failed;
+            }
+
+        }
     }
 
     public RequestStatus editLocation(Location location) {
-        return RequestStatus.Failed;
+        if (locationJdbcRepositoryImpl.findById(location.getId()).isPresent()) {
+            if (locationJdbcRepositoryImpl.update(location)) {
+                System.out.println("the location updated successfully");
+                return RequestStatus.Successful;
+            } else {
+                System.out.println("Something went wrong!");
+                return RequestStatus.Failed;
+            }
+        } else {
+            System.out.println("The location doesn't exist");
+            return RequestStatus.Failed;
+        }
     }
 
     public RequestStatus deleteLocation(Location location) {
-        return RequestStatus.Failed;
+        if (locationJdbcRepositoryImpl.findById(location.getId()).isPresent()) {
+            if (locationJdbcRepositoryImpl.delete(location)) {
+                System.out.println("the location deleted successfully");
+                return RequestStatus.Successful;
+            } else {
+                System.out.println("Something went wrong!");
+                return RequestStatus.Failed;
+            }
+        } else {
+            System.out.println("The location doesn't exist");
+            return RequestStatus.Failed;
+        }
     }
 
-    public Location getLocationData(Long id) {
-        return null;
+    public String getLocationData(Long id) {
+        Optional<Location> locationOptional = locationJdbcRepositoryImpl.findById(id);
+        return locationOptional.map(location -> String.format("""
+                        Location ID : %d
+                        Province : %s
+                        City : %s
+                        Latitude : %s
+                        Longitude : %s
+                        """,
+                location.getId(),
+                location.getProvince(),
+                location.getCity(),
+                location.getLatitude(),
+                location.getLongitude())).orElse("Location doesn't exist");
     }
 
+    public String getAllLocations() {
+        Location location;
+        List<Location> locationList = locationJdbcRepositoryImpl.getAll();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < locationList.size(); i++) {
+            location = locationList.get(i);
+            stringBuilder.append(String.format("""
+                            -- #%02d --
+                            Location ID : %d
+                            Province : %s
+                            City : %s
+                            Latitude : %s
+                            Longitude : %s
+                            
+                            """,
+                    i + 1,
+                    location.getId(),
+                    location.getProvince(),
+                    location.getCity(),
+                    location.getLatitude(),
+                    location.getLongitude()));
+        }
+
+        return stringBuilder.toString();
+    }
 }
