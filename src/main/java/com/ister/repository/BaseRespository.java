d69ca710d1f7d484f241551e63e4a28e6eea5310
hva23 <haskihossein@gmail.com>
2023-09-19 14:10:38 +0330

    Update BaseRespository.java

--- src/main/java/com/ister/repository/BaseRespository.java
+++ src/main/java/com/ister/repository/BaseRespository.java
@@ -1,11 +1,19 @@
 package com.ister.repository;
 
+import com.ister.common.RequestStatus;
 import com.ister.domain.User;
+import com.ister.service.QueryBuilder;
+import org.jetbrains.annotations.NotNull;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-public interface BaseRespository<T,PK> {
+public interface BaseRespository<T, PK> {
     boolean create(T entity);
 
     boolean delete(T entity);
@@ -16,5 +24,39 @@ public interface BaseRespository<T,PK> {
 
     Optional<T> findById(PK id);
 
+    default Object[] read(String tableName, String[] columns, Map<String, String> conditions) {
+        String url = "jdbc:mysql://localhost:8080/Ister";
+        String username = "root";
+        String password = "v@h@bI2442";
 
+        QueryBuilder queryBuilder = new QueryBuilder();
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet;
+
+            String query;
+            Object[] obj = new Object[3];
+
+            //Make a READ query
+            query = queryBuilder.read(tableName, columns, conditions);
+            //Execute query
+            resultSet = statement.executeQuery(query);
+
+            if(resultSet.next()) {
+                obj[0] = resultSet;
+                obj[1] = connection;
+                obj[2] = statement;
+                return obj;
+            } else {
+                resultSet.close();
+                statement.close();
+                connection.close();
+                return null;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return  null;
+        }
+    }
 }
