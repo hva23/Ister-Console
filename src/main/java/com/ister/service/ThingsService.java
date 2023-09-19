bb116c6987ebafe80cfdf797d9eeb878c962ba42
hva23 <haskihossein@gmail.com>
2023-09-19 18:49:49 +0330

    Update ThingsService.java

--- src/main/java/com/ister/service/ThingsService.java
+++ src/main/java/com/ister/service/ThingsService.java
@@ -6,40 +6,60 @@ import com.ister.domain.TelemetryData;
 import com.ister.domain.Things;
 import com.ister.domain.User;
 import com.ister.repository.ThingsInMemoryRepositoryImpl;
-import com.ister.repository.ThingsRepository;
+import com.ister.repository.ThingsJdbcRepositoryImpl;
+import com.sun.source.tree.BreakTree;
 
 import java.util.*;
 
 public class ThingsService {
-    ThingsRepository thingsRepository = new ThingsInMemoryRepositoryImpl();
+    ThingsJdbcRepositoryImpl thingsRepository;
 
+    public ThingsService() {
+        this.thingsRepository = new ThingsJdbcRepositoryImpl("jdbc:mysql://localhost:8080/Ister", "root", "v@h@bI2442");
+    }
     public RequestStatus addThing(Things thing) {
-        Optional<Things> dbThingOptional = thingsRepository.findById(thing.getId());
-        if (dbThingOptional.isPresent()) {
-            System.out.printf("A thing exists with this ID : %d", thing.getId());
+        if (thingsRepository.findById(thing.getId()).isPresent()) {
+            System.out.println("The thing/product already exists\nOperation failed");
             return RequestStatus.Failed;
+        } else {
+            if (thingsRepository.create(thing)) {
+                System.out.println("Thing/Product added successfully");
+                return RequestStatus.Successful;
+            } else {
+                System.out.println("Something went wrong!");
+                return RequestStatus.Failed;
+            }
         }
-        thingsRepository.create(thing);
-        return RequestStatus.Successful;
     }
 
     public RequestStatus editThing(Things thing) {
-        if (thingsRepository.update(thing))
-            return RequestStatus.Successful;
-        else {
-            System.out.println("This thing doesn't exists!");
+        if (thingsRepository.findById(thing.getId()).isPresent()) {
+            if (thingsRepository.update(thing)) {
+                System.out.println("the location updated successfully");
+                return RequestStatus.Successful;
+            } else {
+                System.out.println("Something went wrong!");
+                return RequestStatus.Failed;
+            }
+        } else {
+            System.out.println("The location doesn't exist");
             return RequestStatus.Failed;
         }
     }
 
-    public RequestStatus delete(Things thing) {
-        Optional<Things> dbThingOptional = thingsRepository.findById(thing.getId());
-        if (dbThingOptional.isPresent()) {
-            thingsRepository.delete(thing);
-            return RequestStatus.Successful;
+    public RequestStatus deleteThing(Things thing) {
+        if (thingsRepository.findById(thing.getId()).isPresent()) {
+            if (thingsRepository.delete(thing)) {
+                System.out.println("the thing/product deleted successfully");
+                return RequestStatus.Successful;
+            } else {
+                System.out.println("Something went wrong!");
+                return RequestStatus.Failed;
+            }
+        } else {
+            System.out.println("The thing/product doesn't exist");
+            return RequestStatus.Failed;
         }
-        System.out.println("This thing doesn't exists or something went wrong!");
-        return RequestStatus.Failed;
     }
 
     public Things getThing(String serialNumber) {
@@ -47,6 +67,25 @@ public class ThingsService {
         return thingsOptional.orElse(null);
     }
 
+    public String getThingData(Things thing) {
+       /* return String.format("""
+                            Thing name : %s
+                            Thing ID : %d
+                            Thing serial number : %s
+                            Thing location (latitude, longitude) : %s(%.2f, %.2f)
+                            Thing owner (username, ID) : %s , %s
+                            Telemetry data : %s
+                            """,
+                thing.getName(),
+                thing.getId(),
+                thing.getSerialNumber(),
+                location.getName(),
+                location.getLatitude(),
+                location.getLongitude(),
+                thing.getUser().getUsername(),
+                thing.getUser().getId()/*,
+                telemetryData);*/return null;
+    }
     public String getThingData(String serialNumber) {
         Optional<Things> thingOptional = thingsRepository.findBySerialNumber(serialNumber);
         if (thingOptional.isPresent()) {
@@ -68,7 +107,7 @@ public class ThingsService {
                             Thing ID : %d
                             Thing serial number : %s
                             Thing location (latitude, longitude) : %s(%.2f, %.2f)
-                            Thing owner (username, ID) : %s , %d
+                            Thing owner (username, ID) : %s , %s
                             Telemetry data : %s
                             """,
                     thingOptional.get().getName(),
