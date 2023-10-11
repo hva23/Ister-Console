0e8fa5577356b14e60efd1bb31c0a91f8fa41589
hva23 <haskihossein@gmail.com>
2023-10-11 20:43:10 +0330

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         MenuService menuService = new MenuService();
 
@@ -19,22 +19,6 @@ public class Main {
                 welcome to ister console application
                 note anytime you enter "exit", application will exit.
                 """);
-
         menuService.menu();
-        /*
-        ThingsService thingsService = new ThingsService();
-        System.out.println(thingsService.getThingData("200s-9fad-1dhg"));*//*
-        TelemetryData telemetryData = new TelemetryData();
-        Things things = new Things();
-        things.setId(1L);
-        telemetryData.setId(4L);
-        telemetryData.setData("Status", "Safe");
-        telemetryData.setThing(things);
-        TelemetryDataService telemetryDataService = new TelemetryDataService();
-//        telemetryDataService.addTelemetryData(telemetryData);
-        System.out.println(telemetryDataService.getAllTelemetryData());
-        telemetryDataService.deleteTelemetryData(telemetryData);
-        System.out.println(telemetryDataService.getAllTelemetryData());
-        System.out.println(telemetryDataService.getTelemetryData(4L));*/
     }
 }
